bf056e198b7fdfbdc31d99e22fe47fe9c84a29e7
Jeremy Appiah <jka00003@mix.wvu.edu>
2025-07-22 00:00:27 -0400

    image destination fix

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,108 +1,116 @@
-
 import { ArrowRight, ExternalLink, Github } from "lucide-react";
 
 const projects = [
-  {
-    id: 1,
-    title: "Recipe Finding Web App",
-    description: "A recipe finding web application that helps users discover new recipes based on their preferences.",
-    image: "/public/BoneAppleTeeth.png",
-    tags: ["React", "CSS", "RapidAPI", "HTML"],
-    githubUrl: "https://github.com/WVU-CS230-2024-01-Group08/bone_apple_teeth?tab=readme-ov-file",
-  },
-  {
-    id: 2,
-    title: "Design of a Simple CPU",
-    description:
-      "Designed and implemented an 8-bit CPU with 32 bytes of RAM and three opcodes on the Intel DE10-Lite FPGA using VHDL.",
-    image: "/public/quartus.jpg",
-    tags: ["VHDL", "Quartus Prime", "FPGA(Intel DE10-Lite)"],
-  },
-  {
-    id: 3,
-    title: "Website Portfolio",
-    description:
-      "A personal portfolio website showcasing my projects and skills. (You are currently viewing it!)",
-    image: "/public/portfolio.png",
-    tags: ["React", "Node.js", "Tailwind CSS", "Lucide", "Vite"],
-    githubUrl: "https://github.com/JeremyKAppiah/Portfolio",
-  },
+	{
+		id: 1,
+		title: "Recipe Finding Web App",
+		description:
+			"A recipe finding web application that helps users discover new recipes based on their preferences.",
+		image: "/BoneAppleTeeth.png",
+		tags: ["React", "CSS", "RapidAPI", "HTML"],
+		githubUrl:
+			"https://github.com/WVU-CS230-2024-01-Group08/bone_apple_teeth?tab=readme-ov-file",
+	},
+	{
+		id: 2,
+		title: "Design of a Simple CPU",
+		description:
+			"Designed and implemented an 8-bit CPU with 32 bytes of RAM and three opcodes on the Intel DE10-Lite FPGA using VHDL.",
+		image: "/quartus.jpg",
+		tags: ["VHDL", "Quartus Prime", "FPGA(Intel DE10-Lite)"],
+	},
+	{
+		id: 3,
+		title: "Website Portfolio",
+		description:
+			"A personal portfolio website showcasing my projects and skills. (You are currently viewing it!)",
+		image: "/Portfolio.png",
+		tags: ["React", "Node.js", "Tailwind CSS", "Lucide", "Vite"],
+		githubUrl: "https://github.com/JeremyKAppiah/Portfolio",
+	},
 ];
 
 export const ProjectsSection = () => {
-  return (
-    <section id="projects" className="py-24 px-4 relative">
-      <div className="container mx-auto max-w-5xl">
-        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-          {" "}
-          Featured <span className="text-primary"> Projects </span>
-        </h2>
+	return (
+		<section id="projects" className="py-24 px-4 relative">
+			<div className="container mx-auto max-w-5xl">
+				<h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+					{" "}
+					Featured{" "}
+					<span className="text-primary">
+						{" "}
+						Projects{" "}
+					</span>
+				</h2>
 
-        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
-          Here are some of my recent projects! Each project varies in complexity
-          and showcases different technologies and skills. 
-        </p>
+				<p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+					Here are some of my recent projects! Each project varies in complexity
+					and showcases different technologies and skills.
+				</p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projects.map((project, key) => (
-            <div
-              key={key}
-              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
-            >
-              <div className="h-48 overflow-hidden">
-                <img
-                  src={project.image}
-                  alt={project.title}
-                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
-                />
-              </div>
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+					{projects.map((project, key) => (
+						<div
+							key={key}
+							className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+						>
+							<div className="h-48 overflow-hidden">
+								<img
+									src={project.image}
+									alt={project.title}
+									className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+								/>
+							</div>
 
-              <div className="p-6">
-                <div className="flex flex-wrap gap-2 mb-4">
-                  {project.tags.map((tag) => (
-                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
-                      {tag}
-                    </span>
-                  ))}
-                </div>
+							<div className="p-6">
+								<div className="flex flex-wrap gap-2 mb-4">
+									{project.tags.map((tag) => (
+										<span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+											{tag}
+										</span>
+									))}
+								</div>
 
-                <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
-                <p className="text-muted-foreground text-sm mb-4">
-                  {project.description}
-                </p>
-                <div className="flex justify-between items-center">
-                  <div className="flex space-x-3">
-                    <a
-                      href={project.demoUrl}
-                      target="_blank"
-                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
-                    >
-                      <ExternalLink size={20} />
-                    </a>
-                    <a
-                      href={project.githubUrl}
-                      target="_blank"
-                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
-                    >
-                      <Github size={20} />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+								<h3 className="text-xl font-semibold mb-1">
+									{" "}
+									{project.title}
+								</h3>
+								<p className="text-muted-foreground text-sm mb-4">
+									{project.description}
+								</p>
+								<div className="flex justify-between items-center">
+									<div className="flex space-x-3">
+										<a
+											href={project.demoUrl}
+											target="_blank"
+											className="text-foreground/80 hover:text-primary transition-colors duration-300"
+										>
+											<ExternalLink size={20} />
+										</a>
+										<a
+											href={project.githubUrl}
+											target="_blank"
+											className="text-foreground/80 hover:text-primary transition-colors duration-300"
+										>
+											<Github size={20} />
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
 
-        <div className="text-center mt-12">
-          <a
-            className="cosmic-button w-fit flex items-center mx-auto gap-2"
-            target="_blank"
-            href="https://github.com/JeremyKAppiah"
-          >
-            Check Out My Github <ArrowRight size={16} />
-          </a>
-        </div>
-      </div>
-    </section>
-  );
+				<div className="text-center mt-12">
+					<a
+						className="cosmic-button w-fit flex items-center mx-auto gap-2"
+						target="_blank"
+						href="https://github.com/JeremyKAppiah"
+					>
+						Check Out My Github <ArrowRight size={16} />
+					</a>
+				</div>
+			</div>
+		</section>
+	);
 };
